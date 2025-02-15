a091930ed2952d8095ca1db675adf61f14aae66a
Antoine L <162304112+t0rbenC0rtes@users.noreply.github.com>
2025-02-15 20:01:51 +0100

    removed my ip route

--- server.js
+++ server.js
@@ -21,10 +21,3 @@ app.use("/scores", require("./routes/scores"));
 // Start Server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
-
-app.get("/my-ip", async (req, res) => {
-    const ipResponse = await fetch("https://api64.ipify.org?format=json");
-    const data = await ipResponse.json();
-    res.json({ renderOutboundIP: data.ip });
-  });
-  
